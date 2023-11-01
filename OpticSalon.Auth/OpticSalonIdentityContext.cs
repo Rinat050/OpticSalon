072b3fb7bd6bc3ac0f8825e0dd3cbfda3b746fcb
Rinat050 <safiullin.rinat.05@gmail.com>
2023-11-01 22:40:55 +0300

    Создана и добавлена миграция для добавления аутентификации

--- OpticSalon.Auth/OpticSalonIdentityContext.cs
+++ OpticSalon.Auth/OpticSalonIdentityContext.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using OpticSalon.Auth.Models;
 
 namespace OpticSalon.Auth
 {
-    internal sealed class OpticSalonIdentityContext : IdentityDbContext<OpticSalonUser, OpticSalonRole, int>
+    public sealed class OpticSalonIdentityContext : IdentityDbContext<OpticSalonUser, OpticSalonRole, int>
     {
         public OpticSalonIdentityContext(DbContextOptions options) : base(options)
         {
         }
     }
+
+    public class OpticSalonContextFactory : IDesignTimeDbContextFactory<OpticSalonIdentityContext>
+    {
+        public OpticSalonIdentityContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OpticSalonIdentityContext>();
+            optionsBuilder.UseNpgsql("");
+
+            return new OpticSalonIdentityContext(optionsBuilder.Options);
+        }
+    }   
 }
