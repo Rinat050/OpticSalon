e1655d8827a860afef6071d5e5d958a4fafde4e1
Rinat050 <safiullin.rinat.05@gmail.com>
2023-11-02 20:48:37 +0300

    Добавлены AuthenticationStateProvider и сервисы

--- OpticSalon.Blazor/Program.cs
+++ OpticSalon.Blazor/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
+using OpticSalon.Auth;
 using OpticSalon.Blazor.Data;
+using OpticSalon.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services
+    .AddData(builder.Configuration)
+    .AddAuth(builder.Configuration)
+    .AddScoped<AuthenticationStateProvider, OpticSalon.Blazor.AuthProvider.AuthenticationStateProvider>();
 
 var app = builder.Build();
 
@@ -18,6 +26,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseMiddleware<AuthenticationMiddleware>();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
