7f2cd5f6a73cb7f7aa4f70190ee25059c8392b08
Rinat050 <safiullin.rinat.05@gmail.com>
2023-11-05 15:03:39 +0300

    Добавлена страница регистрации клиента

---
+++ OpticSalon.Blazor/Data/Credentials/ClientRegisterCredentials.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpticSalon.Blazor.Data.Credentials
+{
+    public class ClientRegisterCredentials
+    {
+        [Required(ErrorMessage = "Обязательное поле!")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Обязательное поле!")]
+        public string Surname { get; set; } = null!;
+
+        [Required(ErrorMessage = "Обязательное поле!")]
+        [StringLength(18, MinimumLength = 18, ErrorMessage = "Необходимо 11 символов!")]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = "Обязательное поле!")]
+        [MinLength(10, ErrorMessage = "Минимум 10 символов!"), 
+        MaxLength(150, ErrorMessage = "Максимум 150 символов!")]
+        public string Address { get; set; } = null!;
+
+        [Required(ErrorMessage = "Обязательное поле!")]
+        [EmailAddress(ErrorMessage = "Необходимо ввести почту!")]
+        public string Login { get; set; } = null!;
+
+        [Required(ErrorMessage = "Обязательное поле!")]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", 
+        ErrorMessage = "Пароль должен включать цифры, заглавные, строчные буквы и специальные символы!")]
+        public string Password { get; set; } = null!;
+
+        [Compare(nameof(Password), ErrorMessage = "Пароли не совпадают!")]
+        public string PasswordRepeat { get; set; } = null!;
+    }
+}
