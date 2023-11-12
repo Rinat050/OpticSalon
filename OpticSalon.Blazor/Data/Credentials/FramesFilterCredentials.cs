e58c8c13c0c8d2d330b27497cae1ea6a061c739a
Rinat050 <safiullin.rinat.05@gmail.com>
2023-11-12 16:10:22 +0300

    Добавлен фильтр на страницу каталога

---
+++ OpticSalon.Blazor/Data/Credentials/FramesFilterCredentials.cs
@@ -0,0 +1,14 @@
+﻿using OpticSalon.Domain.Models;
+
+namespace OpticSalon.Blazor.Data.Credentials
+{
+    public class FramesFilterCredentials
+    {
+        public FrameType Type { get; set; } = null!;
+        public FrameMaterial Material { get; set; } = null!;
+        public Brand Brand { get; set; } = null!;
+        public Gender Gender { get; set; } = null!;
+        public Color Color { get; set; } = null!;
+        public ClientPreferences ClientPreferences { get; set; } = null!;
+    }
+}
---
+++ OpticSalon.Domain/Models/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace OpticSalon.Domain.Models
+{
+    public class Gender
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
