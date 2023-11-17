a75eaeef9b5454dd79f044e1ce6cd97a7eadcf54
Rinat050 <safiullin.rinat.05@gmail.com>
2023-11-17 15:26:50 +0300

    Реализованы компоненты страницы заказа

---
+++ OpticSalon.Domain/Consts/RecipeDataConsts.cs
@@ -0,0 +1,14 @@
+﻿namespace OpticSalon.Domain.Consts
+{
+    public static class RecipeDataConsts
+    {
+        public const double MinSph = -8.0;
+        public const double MaxSph = 6.0;
+        public const double MinCyl = -2.0;
+        public const double MaxCyl = 2.0;
+        public const int MinAxis = 0;
+        public const int MaxAxis = 180;
+        public const int MinDp = 51;
+        public const int MaxDp = 75;
+    }
+}
---
+++ OpticSalon.Domain/FilterData/RecipeDataFilter.cs
@@ -0,0 +1,51 @@
+﻿using OpticSalon.Domain.Consts;
+
+namespace OpticSalon.Domain.FilterData
+{
+    public static class RecipeDataFilter
+    {
+        public static List<double> GetSphRange()
+        {
+            return Range(RecipeDataConsts.MinSph, RecipeDataConsts.MaxSph, 0.25).ToList();
+        }
+
+        public static List<double> GetCylRange()
+        {
+            return Range(RecipeDataConsts.MinCyl, RecipeDataConsts.MaxCyl, 0.25).ToList();
+        }
+
+        public static List<int> GetAxisRange()
+        {
+            return Range(RecipeDataConsts.MinAxis, RecipeDataConsts.MaxAxis)
+                    .Select(x => Convert.ToInt32(x)).ToList();
+        }
+
+        public static List<int> GetDpRange()
+        {
+            return Range(RecipeDataConsts.MinDp, RecipeDataConsts.MaxDp)
+                    .Select(x => Convert.ToInt32(x)).ToList();
+        }
+
+        public static List<double> Range(double start, double stop, double step = 1)
+        {
+            if (step == 0)
+                return new List<double>();
+
+            return RangeIterator(start, stop, step);
+        }
+
+        private static List<double> RangeIterator(double start, double stop, double step)
+        {
+            double x = start;
+            var result = new List<double>();
+
+            while (x <= stop)
+            {
+                result.Add(x);
+                x += step;
+            }
+
+            return result;
+        }
+    }
+}
