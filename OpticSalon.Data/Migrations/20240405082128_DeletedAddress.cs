e7a395e21292246cf8c35a157ccecadfb345236a
Rinat050 <safiullin.rinat.05@gmail.com>
2024-04-05 11:22:45 +0300

    Добавлена миграция для удаления адреса в заказе

---
+++ OpticSalon.Data/Migrations/20240405082128_DeletedAddress.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpticSalon.Data.Migrations
+{
+    public partial class DeletedAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "delivery_address",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "lens_tinting_percent",
+                table: "orders");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "delivery_address",
+                table: "orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "lens_tinting_percent",
+                table: "orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- OpticSalon.Data/Models/OrderDb.cs
+++ OpticSalon.Data/Models/OrderDb.cs
@@ -11,9 +11,7 @@
         public int FrameId { get; set; }
         public LensPackageDb LensPackage { get; set; } = null!;
         public int LensPackageId { get; set; }
-        public int LensTintingPercent { get; set; }
         public string ContactPhoneNumber { get; set; } = null!;
-        public string DeliveryAddress { get; set; } = null!;
         public DateTime CreatedDate { get; set; }
         public ClientDb Client { get; set; } = null!;
         public int ClientId { get; set; }
