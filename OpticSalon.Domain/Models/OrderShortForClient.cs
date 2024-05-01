fa03b272004a991fc41388cc52689913ae8d0292
Rinat050 <safiullin.rinat.05@gmail.com>
2024-05-01 15:07:09 +0300

    Добавлена возможность просмотра заказа для клиента

--- OpticSalon.Domain/Models/OrderShortForClient.cs
+++ OpticSalon.Domain/Models/OrderShortForClient.cs
@@ -1,9 +1,12 @@
-﻿namespace OpticSalon.Domain.Models
+﻿using OpticSalon.Domain.Enums;
+
+namespace OpticSalon.Domain.Models
 {
     public class OrderShortForClient
     {
         public int Id { get; set; }
         public string CreatedDate { get; set; } = null!;
         public string ContactPhoneNumber { get; set; } = null!;
+        public OrderStatus Status { get; set; }
     }
 }
