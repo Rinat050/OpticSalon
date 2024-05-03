199a8718e330ad31be1a5303796be72733c5f270
Rinat050 <safiullin.rinat.05@gmail.com>
2024-05-03 20:01:01 +0300

    Реализовано изменение пароля в личном кабинете

---
+++ OpticSalon.Blazor/Data/Credentials/ClientChangePasswordCredentials.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpticSalon.Blazor.Data.Credentials
+{
+    public class ClientChangePasswordCredentials
+    {
+        [Required(ErrorMessage = "Обязательное поле!")]
+        public string OldPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "Обязательное поле!")]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+        ErrorMessage = "Пароль должен включать цифры, заглавные, строчные буквы и специальные символы!")]
+        public string NewPassword { get; set; } = null!;
+    }
+}
