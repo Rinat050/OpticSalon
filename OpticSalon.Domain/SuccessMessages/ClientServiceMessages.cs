d10e640e1fe174d7f7748949a099a56962214194
Rinat050 <safiullin.rinat.05@gmail.com>
2024-05-22 10:29:28 +0300

    Добавлена пагинация

--- OpticSalon.Domain/SuccessMessages/ClientServiceMessages.cs
+++ OpticSalon.Domain/SuccessMessages/ClientServiceMessages.cs
@@ -3,7 +3,7 @@
     public static class ClientServiceMessages
     {
         public const string SuccessCreated = "Клиент успешно добавлен!";
-        public const string SuccessUpdated = "Ваши данные успешно обновлены!";
+        public const string SuccessUpdated = "Данные успешно обновлены!";
         public const string SuccessPreferencesSaving = "Предпочтения успешно сохранены!";
         public const string ClientNotFounded = "Клиент не найден!";
     }
